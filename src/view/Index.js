caa84f24c9693f1245d7e6f13562424b106c988e
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-05-23 18:03:34 -0300

    commit pagina orçamento

--- src/view/Index.js
+++ src/view/Index.js
@@ -1,49 +1,62 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useContext, useEffect } from 'react';
 import { Ionicons, MaterialIcons, Entypo, FontAwesome5 } from '@expo/vector-icons';
+import { Box, Center, Select, CheckIcon, Button, VStack,Text, HStack } from 'native-base';
 
 
-import { AuthContext } from '../controller/auth';
-
+import SelectFunc from '../components/SelectFunc';
+import SelectServico from '../components/SelectServico';
 
 const IndexScreen = () => {
-  const { user, nome } = useContext(AuthContext);
+  const [selectedFunc, setSelectedFunc] = useState('');
+  const [selectedServ, setSelectedServ] = useState('');
+
+
+  const handleSelectFunc = (value) => {
+    setSelectedFunc(value);
+    console.log("ID Servidor:", selectedFunc)
+
+  };
+  const handleSelectServ = (value) => {
+    setSelectedServ(value);
+    console.log("ID Serviço:", selectedServ)
+
+  };
+
+  const handleAgendar = () => {
+    console.log("Agendado")
+
+  };
+
 
   return (
-    <View style={styles.container}>
-      <Text>Criador {nome}</Text>
-      <Text>Seja bem Vindo {user.login}</Text>
+    <Center width="full">
+      <Box>
+        <SelectFunc onValueChange={handleSelectFunc} />
+        <SelectServico  onValueChange={handleSelectServ}/>
+    
+      </Box>
 
+      <Box  minWidth="200">
+        <HStack justifyContent={'space-between'}>
+          <Text color="primary.500" >Servidor:</Text>
+          <Text color="primary.500">{selectedFunc}</Text>
+        </HStack>
 
-    </View>
+        <HStack mt={5} justifyContent={'space-between'}>
+          <Text color="tertiary.400" >Serviço:</Text>
+          <Text color="tertiary.400">{selectedServ}</Text>
+        </HStack>
+      </Box>
+
+      
+
+      <Button size="sm" mt={7} variant="subtle" onPress={() => handleAgendar()}>
+          Agendar
+        </Button>
+    </Center>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  content: {
-    width: '100%',
-    height: 100,
-  },
-  list: {
-    height: '100%',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flex: 1,
-    flexDirection: 'row',
-  },
-  line: {
-    width: '100%',
-    height: 2,
-    backgroundColor: 'grey',
-  },
-  text: {
-    fontSize: 25,
-    fontWeight: '600',
-  },
-});
+
 
 export default IndexScreen;
